7cce7bf5e612afdb575ad436fd10058a5e9c1baf
laurkp <120257523+laurkp@users.noreply.github.com>
2023-03-01 21:51:14 +0000

    Update Program.cs

--- reverseString/Program.cs
+++ reverseString/Program.cs
@@ -9,25 +9,25 @@ namespace reverseString
             // Asking for a string as a input
             Console.Write("Please enter a String : ");
             // Using Console.ReadLine() to read the input string
-            string theString = Console.ReadLine();
+            string inputString = Console.ReadLine();
             // Initializing a string variable to an empty string
             string reverseString = string.Empty;
             // Using a foreach loop to reverse the string
-            foreach (char c in theString)
+            foreach (char c in inputString)
             {
                 reverseString = c + reverseString;
             }
             Console.Write($"Reverse String is : {reverseString} ");
             Console.ReadLine();
             // Using String.Equals() method to check if the string is a palindrome
-            bool equal = String.Equals(theString, reverseString, StringComparison.OrdinalIgnoreCase);
+            bool equal = String.Equals(inputString, reverseString, StringComparison.OrdinalIgnoreCase);
             if (equal == true)
             {
-                Console.WriteLine($"The string {theString} is a palindrome");
+                Console.WriteLine($"The string {inputString} is a palindrome");
             } 
             else
             {
-                Console.WriteLine($"The string {theString} is not a palindrome");
+                Console.WriteLine($"The string {inputString} is not a palindrome");
             }
             Console.ReadLine();
         }
